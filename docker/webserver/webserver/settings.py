53453bff02247f65434d2f7274cc05838c292715
pupio <guicpupio@gmail.com>
2019-11-02 10:19:54 -0300

    Adicionado template PurpleAdmin

--- docker/webserver/webserver/settings.py
+++ docker/webserver/webserver/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES_DIR = BASE_DIR + '/webserver/templates'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'webserver'
+]
+
+
+INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,10 +58,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'webserver.urls'
 
+TEMPLATE_DIRS = [TEMPLATES_DIR,]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+BASE_PATH_MEDIA = os.path.dirname(os.path.dirname(__file__)) + '/static/'
+
+STATICFILES_DIRS = [
+    BASE_PATH_MEDIA,
+]
