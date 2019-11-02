32005929440a609f0ab1c280aa7d6e6c36145d06
pupio <guicpupio@gmail.com>
2019-11-02 10:52:03 -0300

    Remocao de Trechos nao utilizados + Usuario Dinamico

--- docker/webserver/webserver/views.py
+++ docker/webserver/webserver/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'webserver/index.html')
+    context = {
+        'usuario': "Guilherme Pupio"
+    }
+    return render(request, 'webserver/index.html', context)
