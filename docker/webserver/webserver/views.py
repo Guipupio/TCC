a77ba900ec88e2f4453e702def8c5e07469f02d3
pupio <guicpupio@gmail.com>
2019-11-04 20:23:06 -0300

    Adicionei Nova URL

--- docker/webserver/webserver/views.py
+++ docker/webserver/webserver/views.py
@@ -1,7 +1,14 @@
+from os import environ
+
+from django.http import JsonResponse
 from django.shortcuts import render
+
 
 def home(request):
     context = {
         'usuario': "Guilherme Pupio"
     }
     return render(request, 'webserver/index.html', context)
+
+def get_hostname(request):
+    return JsonResponse({'Hostname': environ['HOSTNAME'] } )
