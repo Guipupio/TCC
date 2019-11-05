1d8982b005ebcd9db85e7c3d43d7c1a9b0285591
pupio <guicpupio@gmail.com>
2019-11-04 22:25:20 -0300

    obtendo outras infos do pod

--- docker/webserver/webserver/views.py
+++ docker/webserver/webserver/views.py
@@ -12,7 +12,14 @@ def home(request):
     return render(request, 'webserver/index.html', context)
 
 def get_hostname(request):
-    return JsonResponse({'Hostname': os.environ['HOSTNAME'] } )
+    RAM_INFO = os.popen('free -t -m -h | grep Mem:').readlines()[0]
+    RAM_INFO = [info for info in RAM_INFO.split(' ') if info != '']
+    infos = {
+        'Hostname': os.environ['HOSTNAME'],
+        'CPU Use': '{}%'.format(str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))),
+        'RAM Use':  'Usado: {}; Total: {}'.format(RAM_INFO[2], RAM_INFO[1])
+    }
+    return JsonResponse(infos)
 
 
 def home_json(request):
