3a239fa2cc40fd3c10970e07592b1b4b5a561363
pupio <guicpupio@gmail.com>
2019-11-09 17:39:49 -0300

    Criacao do docker file para o servico requisitor + realizacao de request

--- simulador_requests/bot/bot/views.py
+++ simulador_requests/bot/bot/views.py
@@ -1,7 +1,7 @@
 import os
 from time import time, sleep
 from django.http import JsonResponse
-
+ 
 import numpy as np
 
 import requests
@@ -19,17 +19,40 @@ def get_tempo_exec(func):
     return calcula_tempo
 
 @get_tempo_exec
-def realiza_request(url: str):
-    sleep(0.5)
+def realiza_request(host: str, pagina:str):
+    sleep(0.001)
+    
+    prefixo_url = 'http://'
+    pagina = "/info_twitches"
+    
+    # Realiza Request
+    response = requests.get(prefixo_url + host + pagina)
+    
+    return response.status_code
+    
+    
 
+
+def get_ip(servico:str, type_service: str = "clusterIP") -> str:
+    
+    commando = "kubectl get services/{servico} -o go-template='{abre_chaves}(index .spec.{type_service}){fecha_chaves}'".format(servico=servico, type_service=type_service, abre_chaves= '{{',
+fecha_chaves= '}}')
+    
+    # obtem IP do servico desejado
+    return os.popen(commando).read()
+        
 def simula_request(request):
     
     _request = request.GET if request.method == "GET" else request.POST
     
-    n_iteracoes = _request.get("num_iteracoes", 5)
+    n_iteracoes = _request.get("num_iteracoes", 1000)
     # Obtem IP do servico 1
+    ip_servico = get_ip(servico="consumidor-twitches-svc")
+    
+    # Define pagina acessada
+    pagina = _request.get("pagina", '')
     
     # Realiza Request
-    lista_tempos = [realiza_request('blabla')[1] for i in range(n_iteracoes)]
+    lista_tempos = list(map(lambda x: realiza_request(host=ip_servico, pagina=pagina), list(range(n_iteracoes))))
     
-    return JsonResponse({'tempo_medio': np.array(lista_tempos).mean()})
+    return JsonResponse({'tempo_medio': np.array(lista_tempos)[:,0].mean()})
