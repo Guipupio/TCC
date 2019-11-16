562e7fee2aaa7d95d884a10b5e77e16f3000d964
pupio <guicpupio@gmail.com>
2019-11-15 23:37:47 -0300

    correcao id do card

--- interface/webserver/static/assets/js/webserver/servico1.js
+++ interface/webserver/static/assets/js/webserver/servico1.js
@@ -6,7 +6,7 @@ function post_servico_saudavel() {
         success: function (response) {
             resultado = response;
             $("#ultima_requisicao_saudavel").text(resultado.ultima_requisicao_saudavel);
-            $("#tempo_request_saudavel").text(resultado.tempo_request_saudavel);
+            $("#tempo_medio_request_saudavel").text(resultado.tempo_request_saudavel);
         }
     });
 }
@@ -19,7 +19,7 @@ function post_servico_teste() {
         success: function (response) {
             resultado = response;
             $("#ultima_requisicao_chaos").text(resultado.ultima_requisicao_chaos);
-            $("#tempo_request_chaos").text(resultado.tempo_request_chaos);
+            $("#tempo_medio_request_chaos").text(resultado.tempo_request_chaos);
         }
     });
 }
